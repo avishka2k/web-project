3978cf560142e5c0411002729c71d1b5c62e0151
avishka2k <avishka2k@gmail.com>
2022-01-24 12:43:30 +0530

    landing page create

--- header.php
+++ header.php
@@ -1,22 +1,105 @@
-<div class="navbar">
+<header>
+		<div class="navbar">
     <div class="container">
-      <div class="logo">
+      <div class="logo" id="logo">
         <a href="#">LogoHere</a>
-      </div>
-        <img id="mobile-menu" class="mobile-menu" src="images/mobile-menu.svg">
-
-        <nav>
-            <img id="mobile-close" class="mobile-close" src="images/menu-close.svg">
-            <ul class="primary-nav navlist">
+      </div>      
+        <nav>           
+            <ul class="navlist">
                 <li><a href="index.php">Home</a></li>
-                <li><a href="services.php">Services</a></li>
-                <li><a href="News.php">News</a></li>
-                <li><a href="blog.php">Blog</a></li>
-            </ul>
-            <ul class="secondary-nav navlist">
-                <li class="get-started header-btn"><a href="">Get started</a></li>
-                <li class="register-md header-btn"><a href="includes/register.php">Register as a MD</a></li>
-            </ul>
-        </nav>
+                <li><a href="meals.php">Healthy Meals</a></li>
+                <li><a href="fithit.php">Fit Hit</a></li>
+                <li><a href="aboutus.php">About Us</a></li>
+                <li><a href="contactus.php">Contact Us</a></li>
+                <li class="header-btn"><a href="includes/register.php">Register</a></li>
+        </nav>       
     </div>
 </div>
+	</header>
+	
+    <div class="slider">
+		<!-- fade css -->
+		<div class="myslide fade">
+			<div class="txt">
+				<h1>IMAGE 1</h1>
+				<p>Live mentally & physicaly healthier with Loreum</p>
+                <div class="slide-btn">
+                    <a href="">LET'S WORK</a>
+                    <div class="btn-arrow">
+                        <img src="images/btn-arrow.svg">
+                    </div>
+                </div>
+			</div>
+			<img src="images/hero-01.jpg">
+		</div>
+		
+		<div class="myslide fade">
+			<div class="txt">
+				<h1>Life changing<br/>Diet.</h1>
+				<p>Make a difference in your life with our<br>
+Diet Plan and Job Opportunity!</p>
+                <div class="slide-btn">
+                    <a href="">LET'S WORK</a>
+                    <div class="btn-arrow">
+                        <img src="images/btn-arrow.svg">
+                    </div>
+                </div>
+			</div>
+			<img src="images/hero-02.jpg">
+		</div>
+		
+		<div class="myslide fade">
+			<div class="txt">
+				<h1>IMAGE 3</h1>
+				<p>Web Devoloper<br>Subscribe To My Channel For More Videos</p>
+                <div class="slide-btn">
+                    <a href="">LET'S WORK</a>
+                    <div class="btn-arrow">
+                        <img src="images/btn-arrow.svg">
+                    </div>
+                </div>
+			</div>
+			<img src="images/hero-03.jpg">
+		</div>
+		
+		<div class="myslide fade">
+			<div class="txt">
+				<h1>IMAGE 4</h1>
+				<p>Web Devoloper<br>Subscribe To My Channel For More Videos</p>
+                <div class="slide-btn">
+                    <a href="">LET'S WORK</a>
+                    <div class="btn-arrow">
+                        <img src="images/btn-arrow.svg">
+                    </div>
+                </div>
+			</div>
+			<img src="images/hero-04.jpg">
+		</div>
+		
+		<div class="myslide fade">
+			<div class="txt">
+				<h1>IMAGE 5</h1>
+				<p>Web Devoloper<br>Subscribe To My Channel For More Videos</p>
+                <div class="slide-btn">
+                    <a href="">LET'S WORK</a>
+                    <div class="btn-arrow">
+                        <img src="images/btn-arrow.svg">
+                    </div>
+                </div>
+			</div>
+			<img src="images/hero-05.jpg">
+		</div>
+		<!-- /fade css -->
+		
+		<!-- onclick js -->
+		<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  		<a class="next" onclick="plusSlides(1)">&#10095;</a>
+		
+		<div class="dotsbox" style="text-align:center">
+			<span class="dot" onclick="currentSlide(1)"></span>
+			<span class="dot" onclick="currentSlide(2)"></span>
+			<span class="dot" onclick="currentSlide(3)"></span>
+			<span class="dot" onclick="currentSlide(4)"></span>
+			<span class="dot" onclick="currentSlide(5)"></span>
+		</div>
+	</div>

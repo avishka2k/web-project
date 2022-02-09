2789e1d2ad765966347cbbbd92ddfde6c20e093a
seniyadewmina <seniyadewmina25@gmail.com>
2022-02-09 23:02:42 +0530

    Updated

--- includes/header.php
+++ includes/header.php
@@ -8,12 +8,12 @@
         <nav>    
           <img class="menu-exit menu-btn" id="mobile-exit" src="images/menu-close.svg">   
             <ul class="navlist nav-list">
-                <li><a href="index" class="nav-link">Home</a></li>
-                <li><a href="meals" class="nav-link">Healthy Meals</a></li>
-                <li><a href="fithit" class="nav-link">Fit Hit</a></li>
-                <li><a href="blog" class="nav-link">Blog</a></li>
-                <li><a href="aboutus" class="nav-link">About Us</a></li>
-                <li><a href="contactus" class="nav-link">Contact Us</a></li>
+                <li><a href="index.php" class="nav-link">Home</a></li>
+                <li><a href="meals.php" class="nav-link">Healthy Meals</a></li>
+                <li><a href="fithit.php" class="nav-link">Fit Hit</a></li>
+                <li><a href="blog.php" class="nav-link">Blog</a></li>
+                <li><a href="aboutus.php" class="nav-link">About Us</a></li>
+                <li><a href="contactus.php" class="nav-link">Contact Us</a></li>
                 <br>
                 <div class="header-btn">
                 <a href="includes/register.php">Register</a>

01da942e31464a2a8226d8f35ce2a60b28dba52a
seniyadewmina <seniyadewmina25@gmail.com>
2022-02-20 23:09:29 +0530

    Updated

--- fithit.php
+++ fithit.php
@@ -10,12 +10,7 @@
         <link rel="stylesheet" type="text/css" href="includes/css/header.css">
         <link rel="stylesheet" type="text/css" href="includes/css/ScrollToTop.css">
         <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
-        
-    <style>
-        body {
-    background-color: #E9EDF6;
-        }
-    </style>
+        <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
         
 </head>
 
@@ -24,6 +19,25 @@
  <?php include('includes/header.php'); ?>      
 <!--========Header Section End========-->
 
+<div class="fithit-hero">
+    <div class="fithit-hero-content">
+        <h1>Live mentally & physicaly healthier <br> with FitHit</h1>
+        <p>Once you began to exercise the hardest thing is to stop it.<br> Start today with FitHit</p>
+        <button class="knowmore" style="vertical-align:middle">
+        <span>
+            <a href="" style="color: var(--primary-color);">Know More</a>
+        </span>
+        </button>
+    </div>
+</div>
+<!--========About Section Start========-->
+
+<!--========About Section Start========-->
+
+
+
+<script src="includes/js/fithit.js"></script>
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
 
 <!--========Footer Section Start========-->
 <?php include('includes/footer.php'); ?>
